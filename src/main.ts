f243a21cd48c2db8f8ef21b022f70a1038311532
Todd Walstad <toddwalstad@eaton.com>
2024-06-29 14:44:28 -0400

    Update to use api token

--- src/main.ts
+++ src/main.ts
@@ -1,54 +1,54 @@
-import * as core from '@actions/core'
-import * as github from '@actions/github'
-import * as httpm from '@actions/http-client'
-import {WebhookPayload} from '@actions/github/lib/interfaces'
-import type {WorkflowRunCompletedEvent} from '@octokit/webhooks-types'
+import * as core from "@actions/core";
+import * as github from "@actions/github";
+import * as httpm from "@actions/http-client";
+import { WebhookPayload } from "@actions/github/lib/interfaces";
+import type { WorkflowRunCompletedEvent } from "@octokit/webhooks-types";
 
-const http = new httpm.HttpClient('client')
+const http = new httpm.HttpClient("client");
 
 async function getAccessToken(
   clientId: string,
   clientSecret: string
 ): Promise<string> {
   const response = await http.post(
-    'https://sso.dynatrace.com/sso/oauth2/token',
+    "https://sso.dynatrace.com/sso/oauth2/token",
     `grant_type=client_credentials&client_id=${clientId}&client_secret=${clientSecret}&scope=storage:events:write`,
     {
-      'content-type': 'application/x-www-form-urlencoded'
+      "content-type": "application/x-www-form-urlencoded",
     }
-  )
-  const body = JSON.parse(await response.readBody())
-  return body.access_token as string
+  );
+  const body = JSON.parse(await response.readBody());
+  return body.access_token as string;
 }
 
 function buildCloudEvent(payload: WebhookPayload): unknown {
-  const workflowRun = (payload as WorkflowRunCompletedEvent).workflow_run
+  const workflowRun = (payload as WorkflowRunCompletedEvent).workflow_run;
   return {
-    specversion: '1.0',
+    specversion: "1.0",
     id: `${workflowRun.id}`,
-    type: 'com.dynatrace.github.workflow.run',
-    source: 'dynatrace-workflow-ingester',
+    type: "com.dynatrace.github.workflow.run",
+    source: "dynatrace-workflow-ingester",
     data: {
       ...workflowRun,
       run_duration_ms:
         new Date(workflowRun.updated_at).getTime() -
-        new Date(workflowRun.run_started_at).getTime()
-    }
-  }
+        new Date(workflowRun.run_started_at).getTime(),
+    },
+  };
 }
 
 async function run(): Promise<void> {
   try {
-    const apiToken = core.getInput('dt-api-token'); // Assuming 'dt-api-token' is the name of the input for the API token
-    const environmentId = core.getInput('dt-environment-id');
+    const apiToken = core.getInput("dt-api-token"); // Assuming 'dt-api-token' is the name of the input for the API token
+    const environmentId = core.getInput("dt-environment-id");
     const cloudEvent = buildCloudEvent(github.context.payload);
-    
+
     const response = await http.post(
       `https://${environmentId}.live.dynatrace.com/api/v2/bizevents/ingest`,
       JSON.stringify(cloudEvent),
       {
-        'content-type': 'application/cloudevent+json',
-        authorization: `Api-Token ${apiToken}` // Use the API token directly for authentication
+        "content-type": "application/cloudevent+json",
+        authorization: `Api-Token ${apiToken}`, // Use the API token directly for authentication
       }
     );
     core.info(await response.readBody());
@@ -80,4 +80,4 @@ run();
 //   }
 // }
 
-run()
+run();
